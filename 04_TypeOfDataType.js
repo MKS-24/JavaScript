daddfc5ac34896f7c254a1e929e86ac7e8f6600f
Muhammad Khuzaima Siddiqui <mksprogramify@gmail.com>
2025-02-17 09:21:39 -0800

    Add files via upload

---
+++ 04_TypeOfDataType.js
@@ -0,0 +1,17 @@
+let name = "khan"
+let num = 999999999999999999999999;
+let num1 = 999999999999999999999999n;
+let neww = true;
+let person = {
+    name : "Muhammad Khuzaima Siddiqui",
+    age : 19
+}
+let color = ['red' , 'blue' , 'green' , 55]
+let id = Symbol("idnew")
+console.log(typeof(name))
+console.log(typeof(num))
+console.log(typeof(num1))
+console.log(typeof(neww))
+console.log(typeof(person))
+console.log(typeof(color))
+console.log(typeof(id))
