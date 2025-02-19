76a302751da3c2361dfa141379bd5cbb747025c0
Muhammad Khuzaima Siddiqui <mksprogramify@gmail.com>
2025-02-19 09:14:51 -0800

    Add files via upload

---
+++ 00_Basics/00_Console.js
@@ -0,0 +1,2 @@
+console.log("Shuru Kerta hu Allah Ke Name Se");
+console.log('My Name is "Muhammad Khuzaima Siddiqui"')
---
+++ 00_Basics/01_Let.js
@@ -0,0 +1,19 @@
+// Ager mene kisi variable ko declare kerna hai tu 'let' or 'var' use karuga
+var name = 'Khuzaima'
+console.log(name)
+name = "Muhammad Khuzaima Siddiqui"
+console.log(name)
+
+let name2 = "Ali"
+console.log(name2)
+name2 = "Alsam"
+console.log(name2)
+
+const name3 = "Zakir"
+console.log(name3)
+// name3 = 'Khan' // Error occurs just because of assing value in const variable
+// console.log(name3) // Error occurs just because of assing value in const variable
+
+
+
+// interpreting language hai
