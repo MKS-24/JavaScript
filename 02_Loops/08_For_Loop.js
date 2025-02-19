af13ba0e367544a54c5c5aa799b966fb3de9d462
Muhammad Khuzaima Siddiqui <mksprogramify@gmail.com>
2025-02-19 09:17:35 -0800

    Add files via upload

---
+++ 02_Loops/08_For_Loop.js
@@ -0,0 +1,18 @@
+for(let i = 5;i<=10;i++){
+    console.log(i)
+}
+console.log('new way')
+let i = 5
+for(;i<=10;i++){
+    console.log(i)
+}
+i = 5
+console.log('new way')
+for(;i<=10;){
+    console.log(i)
+    i++
+}
+console.log('new way')
+for(;;){
+    console.log(i)
+}
---
+++ 02_Loops/10_Do_While_loop.js
@@ -0,0 +1,8 @@
+
+let i = 1
+do {
+    if (i==1) console.log("Hello me wityhout condition ke chalgaya")
+    console.log(i)
+i++
+}
+while(i<=5)
